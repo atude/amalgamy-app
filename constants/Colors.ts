49e93aaedcd18690b153edf824e169510a4ba587
Brandon Nguyen <z5205609@student.unsw.edu.au>
2020-11-18 00:19:43 +1100

    Added colour variables

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -16,4 +16,13 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+  brandPurple: '#7442C8',
+  black: '#22252A',
+  subheaderGrey: '#878E95',
+  headerGrey: '#4A5056',
+  grey2: '#AEB5BC',
+  lightgrey3: '#F1F3F5',
+  yellow: '#EECA74',
+  red: '#CC4039'
+
 };
