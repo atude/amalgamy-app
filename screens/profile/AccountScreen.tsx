a797bfc1121ea91ecd1eef5d61b51091cd37ff59
Tanvee Islam <tatsislam@gmail.com>
2020-12-06 22:35:15 +1100

    fixed profile dummy data

--- screens/profile/AccountScreen.tsx
+++ screens/profile/AccountScreen.tsx
@@ -1,64 +1,71 @@
 import * as React from "react";
 import { SubMenu, Option } from "../../types";
 import ProfileMenuSet from "../../components/profile/ProfileMenuSet";
+import { AppContext } from "../../context";
 
-const UserInfo = [
-  {
-    optionText: "Email",
-    pageLink: "ChangeEmailScreen",
-    valueText: "atude@cyberscopic.net",
-  },
-  {
-    optionText: "Change Password",
-    pageLink: "ChangePasswordScreen",
-  },
-];
 
-const RecPrefs: Array<Option> = [
-  {
-    optionText: "Devices and Platforms",
-    pageLink: "DevicesScreen",
-  },
-  {
-    optionText: "Accessiblity",
-    pageLink: "AccessibilityScreen",
-  },
-  {
-    optionText: "Genre",
-    pageLink: "GenreScreen",
-  },
-  {
-    optionText: "Languages",
-    pageLink: "LanguagesScreen",
-  },
-];
-
-const Privacy = [
-  {
-    optionText: "Data Usage",
-    pageLink: "DataUsageScreen",
-  },
-  {
-    optionText: "Privacy Policy",
-    pageLink: "PrivacyPolicyScreen",
-  },
-];
-
-const menus: Array<SubMenu> = [
-  {
-    title: "User Information",
-    optionList: UserInfo,
-  },
-  {
-    title: "Recommendation Preferences",
-    optionList: RecPrefs,
-  },
-  {
-    title: "Privacy and Data",
-    optionList: Privacy,
-  },
-];
 
 export default function AccountScreen() {
-  return <ProfileMenuSet subMenus={menus} />;
+  const generateMenus = () => {
+    const { user } = React.useContext(AppContext);
+
+    const UserInfo = [
+      {
+        optionText: "Email",
+        pageLink: "ChangeEmailScreen",
+        valueText: user?.email,
+      },
+      {
+        optionText: "Change Password",
+        pageLink: "ChangePasswordScreen",
+      },
+    ];
+
+    const RecPrefs: Array<Option> = [
+      {
+        optionText: "Devices and Platforms",
+        pageLink: "DevicesScreen",
+      },
+      {
+        optionText: "Accessiblity",
+        pageLink: "AccessibilityScreen",
+      },
+      {
+        optionText: "Genre",
+        pageLink: "GenreScreen",
+      },
+      {
+        optionText: "Languages",
+        pageLink: "LanguagesScreen",
+      },
+    ];
+
+    const Privacy = [
+      {
+        optionText: "Data Usage",
+        pageLink: "DataUsageScreen",
+      },
+      {
+        optionText: "Privacy Policy",
+        pageLink: "PrivacyPolicyScreen",
+      },
+    ];
+
+    const menus: Array<SubMenu> = [
+      {
+        title: "User Information",
+        optionList: UserInfo,
+      },
+      {
+        title: "Recommendation Preferences",
+        optionList: RecPrefs,
+      },
+      {
+        title: "Privacy and Data",
+        optionList: Privacy,
+      },
+    ];
+    return menus
+  }
+  return <ProfileMenuSet subMenus={generateMenus()} />;
 }
